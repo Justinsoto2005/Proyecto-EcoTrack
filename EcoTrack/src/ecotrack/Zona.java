712963ee370e89f1b8bceb42340390898f4c4ab8
geampierre14 <luisgeampierrei@gmail.com>
2025-11-09 21:57:36 -0500

    Update Zona.java

--- EcoTrack/src/ecotrack/Zona.java
+++ EcoTrack/src/ecotrack/Zona.java
@@ -12,14 +12,26 @@ public class Zona implements Serializable {
         this.nombre = nombre;
     }
 
-    public String getId() { return id; }
-    public String getNombre() { return nombre; }
-    public double getProcesadoKg() { return procesadoKg; }
-    public double getPendienteKg() { return pendienteKg; }
+    public String getId() { 
+        return id; 
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public double getProcesadoKg() {
+        return procesadoKg; 
+    }
+    public double getPendienteKg() {
+        return pendienteKg; 
+    }
 
-    public void addProcesado(double kg) { this.procesadoKg += kg; }
-    public void addPendiente(double kg) { this.pendienteKg += kg; }
-
+    public void addProcesado(double kg) {
+        this.procesadoKg += kg;
+    }
+    public void addPendiente(double kg) { 
+        this.pendienteKg += kg; 
+    }
+    
     public double utilidadAmbiental() {
         return procesadoKg - pendienteKg;
     }
