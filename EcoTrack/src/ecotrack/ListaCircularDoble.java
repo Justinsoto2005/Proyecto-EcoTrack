14d3b37fd13d2f75afae6b419e2ab9d804f90b84
Justinsoto2005 <justinsotoramos2005@gmail.com>
2025-11-09 20:59:54 -0500

    Proyecto Ecotrack

---
+++ EcoTrack/src/ecotrack/ListaCircularDoble.java
@@ -0,0 +1,61 @@
+package ecotrack;
+
+public class ListaCircularDoble {
+    private class Nodo {
+        public Residuo dato;
+        public Nodo siguiente;
+        public Nodo anterior;
+
+        public Nodo(Residuo dato) {
+            this.dato = dato;
+            // Por defecto, un nodo nuevo apunta a sí mismo en una lista circular
+            this.siguiente = this; 
+            this.anterior = this;
+        }
+    }
+
+    private Nodo head;
+    private int size; 
+
+    public ListaCircularDoble() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addLast(Residuo residuo) {
+        Nodo nuevoNodo = new Nodo(residuo);
+
+        if (isEmpty()) {
+            head = nuevoNodo;
+        } else {
+            Nodo tail = head.anterior;
+
+            nuevoNodo.siguiente = head;
+            nuevoNodo.anterior = tail;
+
+            tail.siguiente = nuevoNodo;
+            head.anterior = nuevoNodo;
+        }
+        
+        size++;
+    }
+
+    public Residuo getFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        return head.dato;
+    }
+
+    public Nodo getHeadNode() {
+        return this.head;
+    }
+}
